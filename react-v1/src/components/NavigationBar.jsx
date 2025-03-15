9fc27d52bca2def5a975898b8e3650c3d325c5c4
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-15 12:35:43 +0800

    add header semantic html element with aria-label
    - it improves accessibility for users with disabilities
    - and enhances SEO by providing better structure

--- react-v1/src/components/NavigationBar.jsx
+++ react-v1/src/components/NavigationBar.jsx
@@ -6,27 +6,39 @@ import Col from "react-bootstrap/Col";
 
 function NavigationBar() {
   return (
-    <Container fluid className="p-3 shadow custom-container">
-      <Row className="d-flex justify-content-center">
-        <Col xs={12} md={10} lg={8}>
-          <Navbar expand="lg" className="bg-body-tertiary">
-            <Container fluid>
-              <Navbar.Brand href="#home" className="fs-4">
-                Santé <span className="fw-bold">Barley</span>
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mx-auto">
-                  <Nav.Link href="#home">About Us</Nav.Link>
-                  <Nav.Link href="#link">Branches</Nav.Link>
-                  <Nav.Link href="#link">Contact Us</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </Col>
-      </Row>
-    </Container>
+    <header>
+      <Container fluid className="p-3 shadow custom-container">
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} md={10} lg={8}>
+            <Navbar expand="lg" className="bg-body-tertiary">
+              <Container fluid>
+                <Navbar.Brand
+                  href="#home"
+                  className="fs-4"
+                  aria-label="Go to home"
+                >
+                  Santé <span className="fw-bold">Barley</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mx-auto">
+                    <Nav.Link href="#about-us" aria-label="About us page">
+                      About Us
+                    </Nav.Link>
+                    <Nav.Link href="#branches" aria-label="Branches page">
+                      Branches
+                    </Nav.Link>
+                    <Nav.Link href="#contact-us" aria-label="Contact us page">
+                      Contact Us
+                    </Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
+    </header>
   );
 }
 
