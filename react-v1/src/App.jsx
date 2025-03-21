8a5da2916d917fa508f34ec34778d40d05c71527
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-21 12:16:52 +0800

    render the testimonials component

--- react-v1/src/App.jsx
+++ react-v1/src/App.jsx
@@ -1,6 +1,7 @@
 import GlobalOffices from "./components/GlobalOffices";
 import MobileBanner from "./components/MobileBanner";
 import NavigationBar from "./components/NavigationBar";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavigationBar />
       <MobileBanner />
       <GlobalOffices />
+      <Testimonials />
     </div>
   );
 }
