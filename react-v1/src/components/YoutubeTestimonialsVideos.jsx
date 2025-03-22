78157086bc6172ff7980b41dfec6062b8c5a1dae
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-22 14:29:28 +0800

    render PCOS component

--- react-v1/src/components/YoutubeTestimonialsVideos.jsx
+++ react-v1/src/components/YoutubeTestimonialsVideos.jsx
@@ -1,6 +1,7 @@
 import Diabetes from "./Diabetes";
 import KidneyFailure from "./KidneyFailure";
 import Lupus from "./Lupus";
+import PCOS from "./PCOS";
 
 function YoutubeTestimonialsVideos() {
   return (
@@ -8,6 +9,7 @@ function YoutubeTestimonialsVideos() {
       <Diabetes link={"https://youtu.be/irh5TD_hJek"} />
       <KidneyFailure link={"https://youtu.be/DloCJeNx-eQ"} />
       <Lupus link={"https://youtu.be/3c7XRAy37pY"} />
+      <PCOS link={"https://youtu.be/_aIn6tbQjoI"} />
     </div>
   );
 }
