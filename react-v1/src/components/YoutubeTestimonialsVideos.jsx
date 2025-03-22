8546195f123c0cc47f124be53a96d9d63fe9a303
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-22 14:20:40 +0800

    render kidnet failure component

--- react-v1/src/components/YoutubeTestimonialsVideos.jsx
+++ react-v1/src/components/YoutubeTestimonialsVideos.jsx
@@ -1,9 +1,11 @@
 import Diabetes from "./Diabetes";
+import KidneyFailure from "./KidneyFailure";
 
 function YoutubeTestimonialsVideos() {
   return (
     <div>
       <Diabetes link={"https://youtu.be/irh5TD_hJek"} />
+      <KidneyFailure link={"https://youtu.be/DloCJeNx-eQ"} />
     </div>
   );
 }
