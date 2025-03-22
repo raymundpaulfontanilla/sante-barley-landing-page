4df003633465b2d1006fc86cd8fa00186dab2ccc
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-22 16:45:03 +0800

    add youtube optimization

--- react-v1/src/components/PCOS.jsx
+++ react-v1/src/components/PCOS.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function PCOS({ link }) {
-  const embedUrl = link.replace("youtu.be/", "www.youtube.com/embed/");
+  const videoId = link.split("youtu.be/")[1] || link.split("v=")[1];
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div>
       <Row>
         <Col xs={12} md={8} lg={10}>
-          <iframe
-            width={"100%"}
-            height={"315"}
-            src={embedUrl}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          {!isPlaying ? (
+            <img
+              src={`https://img.youtube.com/vi/${videoId}/hqdefault.jpg`}
+              alt="YouTube Thumbnail"
+              style={{ cursor: "pointer", width: "100%" }}
+              onClick={() => setIsPlaying(true)}
+            />
+          ) : (
+            <iframe
+              width="100%"
+              height="315"
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          )}
         </Col>
       </Row>
     </div>
