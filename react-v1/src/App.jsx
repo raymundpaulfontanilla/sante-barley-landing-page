d272229845d0148e1284a7055f2c0a6f8125467a
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-26 17:33:46 +0800

    render the product component with the use of route

--- react-v1/src/App.jsx
+++ react-v1/src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./components/pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./components/pages/Product";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
