ffc9004be74c147511819211502fd1a8bd6be069
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-26 17:27:34 +0800

    add product heading component

---
+++ react-v1/src/components/pages/ProductHeading.jsx
@@ -0,0 +1,13 @@
+function ProductHeading({ productHeading, productHeadingTwo }) {
+  return (
+    <>
+      <h1 className="d-flex justify-content-center">
+        {productHeading}
+        {"\u00A0"}
+        <span className="fw-bold">{productHeadingTwo}</span>
+      </h1>
+    </>
+  );
+}
+
+export default ProductHeading;
