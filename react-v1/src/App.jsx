d82d2a16c2ad1c246884a04b09890d31c9a850c2
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-27 12:32:12 +0800

    add route for branch page

--- react-v1/src/App.jsx
+++ react-v1/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./components/pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./components/pages/Product";
+import Branch from "./components/pages/Branch";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Product />} />
+        <Route path="/branches" element={<Branch />} />
       </Routes>
     </BrowserRouter>
   );
