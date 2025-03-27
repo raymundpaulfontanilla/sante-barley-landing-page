03509fb14d573856cfdee1e2984af00b94888eb0
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-28 07:29:13 +0800

    add contact us page

---
+++ react-v1/src/components/pages/ContactUs.jsx
@@ -0,0 +1,18 @@
+import NavigationBar from "../NavigationBar";
+import ContactUsHeading from "./ContactUsHeading";
+import ContactUsData from "./ContactUsData";
+
+function ContactUs() {
+  return (
+    <>
+      <NavigationBar />
+      <ContactUsHeading contactUsHeading={"Contact Us"} />
+      <ContactUsData
+        mobileNumber={"Cellphone: 0935-913-8967"}
+        facebookProfile={"https://www.facebook.com/elizabeth.fontanilla.345364"}
+      />
+    </>
+  );
+}
+
+export default ContactUs;
