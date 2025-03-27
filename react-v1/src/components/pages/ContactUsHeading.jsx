561b326218859df650e6df9f57687ab8acbed9dd
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-28 07:30:01 +0800

    add contact us heading

---
+++ react-v1/src/components/pages/ContactUsHeading.jsx
@@ -0,0 +1,12 @@
+function ContactUsHeading({ contactUsHeading }) {
+  return (
+    <>
+      <h1 className="d-flex justify-content-center">
+        {contactUsHeading}
+        {"\u00A0"}
+      </h1>
+    </>
+  );
+}
+
+export default ContactUsHeading;
