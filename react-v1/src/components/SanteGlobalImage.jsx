f83fee51b261b4b367557158d0b97881265e4183
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-03-28 09:41:29 +0800

    add sante global image component

---
+++ react-v1/src/components/SanteGlobalImage.jsx
@@ -0,0 +1,12 @@
+import SanteBarleyGlobe from "../assets/images/sante-barley-globe.png";
+import Image from "react-bootstrap/Image";
+
+function SanteGlobalImage() {
+  return (
+    <>
+      <Image src={SanteBarleyGlobe} alt="Sante Barley Globe" fluid />
+    </>
+  );
+}
+
+export default SanteGlobalImage;
